700bcf8db5d16b0905ab297a06a6d8eabc46dcef
kihwankim <clack2933@gmail.com>
2019-10-10 23:56:31 +0900

    refactoring

--- src/ipc/IPLayer.java
+++ src/ipc/IPLayer.java
@@ -1,7 +1,8 @@
 package ipc;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
-
 
 public class IPLayer implements BaseLayer {
     public int nUpperLayerCount = 0;
@@ -15,6 +16,7 @@ public class IPLayer implements BaseLayer {
     public IPLayer(String pName) {
         pLayerName = pName;
 
+        SetIpSrcAddress(GetIpAddress());//내 IP주소 세팅
     }
 
     public byte[] RemoveCappHeader(byte[] input, short length) {
@@ -25,25 +27,19 @@ public class IPLayer implements BaseLayer {
     }
 
     public boolean Send(byte[] input, short length) {
-
         byte[] temp = ObjToByte20(this.ip_header, input, length);
-        // if(IsCaching) ARP���̺� ĳ�̵Ǿ������� EthernetLayer send ȣ��
-        // return this.GetUnderLayer(0).send(temp, length+20);
 
-        // else ĳ�̵����� ������ ARP Layer send ȣ��
-        // return this.GetUnderLayer(1).send(temp, length+20);
-
-        return false;
+        return this.GetUnderLayer().Send(temp, length + 20);    //ARP Layer에게 전달
     }
 
     public byte[] ObjToByte20(_IP_Header ip_header, byte[] input, short length) {
-        int packet_tot_len = length + 20;
-        byte[] buf = new byte[packet_tot_len];
+
+        byte[] buf = new byte[length + 20];
 
         buf[0] = ip_header.ip_verlen;
         buf[1] = ip_header.ip_tos;
-        buf[2] |= (byte) ((packet_tot_len >> 8) & 0xFF);
-        buf[3] |= (byte) (packet_tot_len & 0xFF);
+        buf[2] |= (byte) ((length >> 8) & 0xFF);
+        buf[3] |= (byte) (length & 0xFF);
 
         buf[4] |= (byte) ((ip_header.ip_id >> 8) & 0xFF);
         buf[5] |= (byte) (ip_header.ip_id & 0xFF);
@@ -64,50 +60,31 @@ public class IPLayer implements BaseLayer {
     }
 
     public synchronized boolean Receive(byte[] input) {
-        // IP Ÿ�� üũ
+        // IP 타입 체크
         if (this.ip_header.ip_verlen != input[0] || this.ip_header.ip_tos != input[1]
-                || this.ip_header.ip_id != (short) (input[4] & 0xFF00) + (short) ((input[5] & 0xFF))
+                || this.ip_header.ip_id != (short) ((input[4] & 0xFF) << 8 + input[5] & 0xFF)
                 || this.ip_header.ip_proto != input[9])
             return false;
 
 
-        byte[] src_ip_addr = new byte[4];
-        byte[] dst_ip_addr = new byte[4];
-        short packet_tot_len = (short) (((input[2] & 0xFF) << 8) + ((input[3]) & 0xFF)); //��Ŷ �� ����
+        for (int i = 0; i < 4; i++)
+            if (this.ip_header.ip_srcaddr.addr[i] != input[16 + i]) { //input의 target IP주소가 나의 IP주소와 일치하는지 확인
+                //return this.GetUnderLayer().proxySend(input, tot_len); //일치하지 않으면 프록시
+            }
 
-        byte[] temp = RemoveCappHeader(input, (short) (input[2] & 0xFF00 + input[3] & 0xFF));
-        System.arraycopy(input, 12, src_ip_addr, 0, 4);
-        System.arraycopy(input, 16, dst_ip_addr, 0, 4);
-
-        // ������ IP�ּҰ� ���̺� ĳ�̵Ǿ����� ��(���Ͻ�)
-        /*
-         * if(caching(dst_ip_addr)) {
-         *
-         * return this.Send(temp, packet_tot_len-20); }
-         *
-         *
-         * //�� IP�ּҿ� ��ġ�ϴ��� Ȯ�� for (int i = 0; i < 4; i++) { //����ġ
-         *  if (dst_ip_addr[i] !=
-         * this.ip_header.ip_srcaddr.addr[i]) {
-         * return false;
-         *
-         * }
-         *
-         * //��ġ return this.Receive(temp);
-         * }
-         */
-        return true;
+        //일치하면 demultiplex하고 상위 레이어로 올림
+        short tot_len = (short) (((input[2] & 0xFF) << 8) + input[3] & 0xFF);
+        byte[] temp = RemoveCappHeader(input, tot_len);
+        return this.GetUpperLayer(0).Receive(temp);
     }
 
     @Override
     public String GetLayerName() {
-        // TODO Auto-generated method stub
         return pLayerName;
     }
 
     @Override
     public BaseLayer GetUnderLayer() {
-        // TODO Auto-generated method stub
         if (p_UnderLayer == null)
             return null;
         return p_UnderLayer;
@@ -115,7 +92,6 @@ public class IPLayer implements BaseLayer {
 
     @Override
     public BaseLayer GetUpperLayer(int nindex) {
-        // TODO Auto-generated method stub
         if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
             return null;
         return p_aUpperLayer.get(nindex);
@@ -123,7 +99,6 @@ public class IPLayer implements BaseLayer {
 
     @Override
     public void SetUnderLayer(BaseLayer pUnderLayer) {
-        // TODO Auto-generated method stub
         if (pUnderLayer == null)
             return;
         this.p_UnderLayer = pUnderLayer;
@@ -144,35 +119,50 @@ public class IPLayer implements BaseLayer {
         pUULayer.SetUnderLayer(this);
     }
 
-    // src IP�ּ� ����
+    // src IP주소 세팅
     public void SetIpSrcAddress(byte[] srcAddress) {
-        // TODO Auto-generated method stub
         ip_header.ip_srcaddr.addr = srcAddress;
     }
 
-    // dst IP�ּ� ����
+    // dst IP주소 세팅
     public void SetIpDstAddress(byte[] dstAddress) {
-        // TODO Auto-generated method stub
         ip_header.ip_dstaddr.addr = dstAddress;
 
     }
 
-    // Header �ڷᱸ��
+    public byte[] GetIpAddress() {
+
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String[] hostAddressInString = local.getHostAddress().split("[.]");
+
+        byte[] temp = new byte[4];
+        for (int i = 0; i < 4; i++)
+            temp[i] = Byte.parseByte(hostAddressInString[i]);
+
+        return temp;
+    }
+
+    // Header 자료구조
     private class _IP_Header {
-        byte ip_verlen; // ip version (1byte)
-        byte ip_tos; // type of service (1byte)
-        short ip_len; // total packet length (2byte)
-        short ip_id; // datagram Identification(2byte)
-        short ip_fragoff;// fragment offset (2byte)
-        byte ip_ttl; // time to live in gateway hops (1byte)
-        byte ip_proto; // IP protocol (1byte)
-        short ip_cksum; // header checksum (2byte)
+        byte ip_verlen; // ip version (1byte)   index 0
+        byte ip_tos; // type of service (1byte) index 1
+        short ip_len; // total packet length (2byte) index 2~3
+        short ip_id; // datagram Identification(2byte) index 4~5
+        short ip_fragoff;// fragment offset (2byte)	index 6~7
+        byte ip_ttl; // time to live in gateway hops (1byte) index 8
+        byte ip_proto; // IP protocol (1byte) index 9
+        short ip_cksum; // header checksum (2byte) index 10 11
         // 0~11 index
 
-        _IP_ADDR ip_dstaddr;// destination IP address (4byte) 12~15 index
-        _IP_ADDR ip_srcaddr;// source IP address (4byte) 16~19 index
+        _IP_ADDR ip_srcaddr;// source IP address (4byte) 12~15 index
+        _IP_ADDR ip_dstaddr;// destination IP address (4byte) 16~19 index
 
-        // char ip_data[]; //variable length data (variable)
+        // byte ip_data[]; //variable length data (variable)
 
         public _IP_Header() {
             this.ip_verlen = 4;
@@ -183,12 +173,12 @@ public class IPLayer implements BaseLayer {
             this.ip_ttl = 0;
             this.ip_proto = 0;
             this.ip_cksum = 0;
-            this.ip_dstaddr = new _IP_ADDR();
             this.ip_srcaddr = new _IP_ADDR();
+            this.ip_dstaddr = new _IP_ADDR();
 
         }
 
-        // ����� IP�ּ� �ڷᱸ��
+        // 헤더의 IP주소 자료구조
         private class _IP_ADDR {
             private byte[] addr = new byte[4];
 
@@ -197,9 +187,7 @@ public class IPLayer implements BaseLayer {
                 this.addr[1] = 0;
                 this.addr[2] = 0;
                 this.addr[3] = 0;
-
             }
-
         }
     }
 }
